fc0f7803f34af76eba0d4e0722e780ff939ac9fa
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-01-27 15:26:25 -0300

    Routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from "react"
+import type React from "react"
 import {
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom"
+
 import Home from "./components/Templates/Home"
 import RestaurantDetails from "./components/Templates/RestaurantDetails"
-// import Navbar from "./components/Molecules/Navbar"
-// import Footer from "./components/Molecules/Footer"
-{
-  /* <Navbar />
-<Footer /> */
-}
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-      <Route path="/restaurant/:id" element={<RestaurantDetails />} />
-    </Route>,
-  ),
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/restaurant",
+        element: <RestaurantDetails />,
+      },
+    ],
+  },
+])
 
 const App: React.FC = () => {
   return <RouterProvider router={router} />
