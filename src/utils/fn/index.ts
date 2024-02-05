42297483b78d0ae4dd7e5ffde7996e333cac71a6
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-02-05 17:06:15 -0300

    Remove unused function

--- src/utils/fn/index.ts
+++ src/utils/fn/index.ts
@@ -1,5 +1,3 @@
-import { useFormikContext } from 'formik';
-
 const parseToBrl = (amount = 0) => {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
@@ -13,14 +11,5 @@ const getTotalPrice = (pratos: ICart[]) => {
   }, 0)
 }
 
-const useCheckInputHasError = (fieldName: string) => {
-  const { touched, errors } = useFormikContext()
-  const isTouched = fieldName in touched
-  const isInvalid = fieldName in errors
-  const hasError = isTouched && isInvalid
 
-  return hasError
-}
-
-
-export { parseToBrl, getTotalPrice, useCheckInputHasError }
+export { parseToBrl, getTotalPrice }
