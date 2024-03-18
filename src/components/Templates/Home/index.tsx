c6bd88673db10d3ab7531fcd1983c7448859df85
Arthur-Ferreira <arthurfdarosa@gmail.com>
2024-03-18 15:40:59 -0300

    Feat Refactor: Remove Footer that already is present at Root Layout

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -2,24 +2,21 @@ import type React from "react"
 
 import Card from "../../Molecules/Card"
 import Header from "../../Organisms/Menu"
-import Footer from "../../Organisms/Footer"
 
 import CardList from "./styles"
 import { MainContainer } from "../styles"
 import { useGetRestaurantsQuery } from "../../../utils/api"
-import Loader from "../../Atoms/Loader"
 
 const Home: React.FC = () => {
-  const { data: restaurants, isLoading } = useGetRestaurantsQuery()
+  const { data: restaurants } = useGetRestaurantsQuery()
 
   if (!restaurants) return null
 
-    return (
+  return (
     <>
       <Header />
       <MainContainer>
         <CardList>
-          {isLoading && <Loader />}
           {restaurants.map(restaurant => (
             <li key={restaurant.id}>
               <Card restaurant={restaurant} />
@@ -27,7 +24,6 @@ const Home: React.FC = () => {
           ))}
         </CardList>
       </MainContainer>
-      <Footer />
     </>
   )
 }
